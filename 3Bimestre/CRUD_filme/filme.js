ce4324c2a3d5afdb8a233f9625dc3016c2af81fe
Luana Almeida Cazarin <luana.cazarin.almeida@gmail.com>
2026-08-11 11:50:21 -0300

    11/08/2026

---
+++ 3Bimestre/CRUD_filme/filme.js
@@ -0,0 +1,10 @@
+class Filme {
+    constructor(id, nome, modelo,ano, posicaoNaLista) {
+        this.id = id;
+        this.nome = nome;
+         this.modelo = modelo;
+         this.ano = ano;
+
+        this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+    }
+}
